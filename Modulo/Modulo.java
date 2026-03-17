3df6a1c26e1c49bb361ad423eb8920081c0cfb72
Sumit Nikit <sumitnikitsumnik@gmail.com>
2026-03-18 01:01:58 +0530

    Modular Arithmatic

--- Modulo/Modulo.java
+++ Modulo/Modulo.java
@@ -34,4 +34,34 @@ public class Modulo{
         int n =nextInt();
         sb.append(n%5);
     }
+    public static int power(int a, int b){
+        if(b==0)return 1;
+        if(b%2==1) {
+            return a*power(a,b-1);
+        }else{
+            int temp = power(a,b/2);
+            return temp*=temp;
+        }
+        
+    }
+    public static int power1(int a, int b){
+        if(b==0)return 1;
+        int temp =power(a,b/2);
+        temp*=temp;
+        if(b%2==1){
+            temp = a*temp;
+        }
+        return temp;
+        /*static long add(long a, long b) { return (a+b) % MOD; }
+static long sub(long a, long b) { return (a-b+MOD) % MOD; }
+static long mul(long a, long b) { return a%MOD * (b%MOD) % MOD; }
+static long div(long a, long b) { return mul(a, modInverse(b, MOD)); }
+static long modInverse(long a)  { return power(a, MOD-2, MOD); }
+static long power(long b, long e, long m) {
+    long r = 1L;
+    for (b %= m; e > 0; e >>= 1, b = b*b%m)
+        if ((e&1)==1) r = r*b%m;
+    return r;
+} */
+    }
 }
